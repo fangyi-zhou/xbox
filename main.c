31472c1ef766ecbae964837c4b675dfdfdb3e8b9
Fangyi Zhou <me@fangyi.io>
2021-11-09 11:05:56 +0000

    Draw a box

--- main.c
+++ main.c
@@ -4,15 +4,22 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
 
+#define BOX_WIDTH 100
+#define BOX_HEIGHT 100
+#define BOX_INIT_X 35
+#define BOX_INIT_Y 65
+#define BOX_BG_DIFF_X 30
+#define BOX_BG_DIFF_Y (-30)
+
 int main(int argc, char **argv) {
   Display *display = XOpenDisplay(NULL);
   if (!display) {
+    perror("Cannot open display");
     return 1;
   }
-  printf("%s", "Opened display\n");
   int blackColor = BlackPixel(display, DefaultScreen(display));
   int whiteColor = WhitePixel(display, DefaultScreen(display));
-  Window w = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 200, 100, 0, blackColor, blackColor);
+  Window w = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 200, 200, 0, blackColor, blackColor);
   XSelectInput(display, w, StructureNotifyMask);
   XMapWindow(display, w);
   GC gc = XCreateGC(display, w, 0, NULL);
@@ -24,7 +31,18 @@ int main(int argc, char **argv) {
       break;
     }
   }
-  XDrawLine(display, w, gc, 10, 60, 180, 20);
+  // Draw the foreground box
+  XDrawLine(display, w, gc, BOX_INIT_X, BOX_INIT_Y, BOX_INIT_X + BOX_WIDTH, BOX_INIT_Y);
+  XDrawLine(display, w, gc, BOX_INIT_X, BOX_INIT_Y, BOX_INIT_X, BOX_INIT_Y + BOX_HEIGHT);
+  XDrawLine(display, w, gc, BOX_INIT_X + BOX_WIDTH, BOX_INIT_Y, BOX_INIT_X + BOX_WIDTH, BOX_INIT_Y + BOX_HEIGHT);
+  XDrawLine(display, w, gc, BOX_INIT_X, BOX_INIT_Y + BOX_HEIGHT, BOX_INIT_X + BOX_WIDTH, BOX_INIT_Y + BOX_HEIGHT);
+  // Draw the background box
+  XDrawLine(display, w, gc, BOX_INIT_X + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_BG_DIFF_Y, BOX_INIT_X + BOX_WIDTH + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_BG_DIFF_Y);
+  XDrawLine(display, w, gc, BOX_INIT_X + BOX_WIDTH + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_BG_DIFF_Y, BOX_INIT_X + BOX_WIDTH + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_HEIGHT + BOX_BG_DIFF_Y);
+  // Connect the foreground and background
+  XDrawLine(display, w, gc, BOX_INIT_X, BOX_INIT_Y, BOX_INIT_X + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_BG_DIFF_Y);
+  XDrawLine(display, w, gc, BOX_INIT_X + BOX_WIDTH, BOX_INIT_Y, BOX_INIT_X + BOX_WIDTH + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_BG_DIFF_Y);
+  XDrawLine(display, w, gc, BOX_INIT_X + BOX_WIDTH, BOX_INIT_Y + BOX_HEIGHT, BOX_INIT_X + BOX_WIDTH + BOX_BG_DIFF_X, BOX_INIT_Y + BOX_HEIGHT + BOX_BG_DIFF_Y);
   XFlush(display);
   sleep(10);
   return 0;
